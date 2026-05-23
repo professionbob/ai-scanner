e7f2181d2300fd49327fbff12ba78768c8b5ced9
professionbob <professionbob@gmail.com>
2026-05-24 07:36:06 +0800

    Update positions.py

--- positions.py
+++ positions.py
@@ -2,11 +2,16 @@ POSITIONS = {
     "ONDS": {
         "avg_cost": 10.16,
         "shares": 11373,
-        "style": "trend"
+        "style": "trend",
+        "add_times": 0,
+        "max_add_times": 2
     },
+
     "NBIS": {
         "avg_cost": 177.08,
         "shares": 17,
-        "style": "trend"
+        "style": "trend",
+        "add_times": 0,
+        "max_add_times": 2
     }
 }
