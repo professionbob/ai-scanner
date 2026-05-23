1c18b4cf54de0a4c43f19e7990f8045b06bcd40a
professionbob <professionbob@gmail.com>
2026-05-23 23:38:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from datetime import datetime, timedelta
 from ta.momentum import RSIIndicator
 from ta.trend import MACD
 
-BOT_TOKEN = "你的Bot Token"
+BOT_TOKEN = "8846284007:AAEZz4f50N8g1JcC6P8Z2ujcA2hx32-gv5A
+"
 CHAT_ID = "8851496243"
 
 SCAN_INTERVAL = 300
