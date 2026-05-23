c07225840977af826eccbef181454eedea9dc7a1
professionbob <professionbob@gmail.com>
2026-05-24 07:41:26 +0800

    Create trades.py

---
+++ trades.py
@@ -0,0 +1,17 @@
+TRADES = [
+    {
+        "date": "2026-05-20",
+        "ticker": "ONDS",
+        "action": "BUY",
+        "shares": 11373,
+        "price": 10.16
+    },
+
+    {
+        "date": "2026-05-20",
+        "ticker": "NBIS",
+        "action": "BUY",
+        "shares": 17,
+        "price": 177.08
+    }
+]
