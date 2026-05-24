6aa67325afa9f4dac17b629a6f6adad916e5572d
professionbob <professionbob@gmail.com>
2026-05-24 13:34:10 +0800

    Create narrative_engine.py

---
+++ narrative_engine.py
@@ -0,0 +1,21 @@
+NARRATIVE_KEYWORDS = {
+
+    "GLASS_SUBSTRATE": [
+        "glass substrate",
+        "TGV",
+        "LIDE",
+        "panel level packaging",
+    ],
+
+    "OPTICAL": [
+        "silicon photonics",
+        "optical interconnect",
+        "CPO",
+    ],
+
+    "HBM": [
+        "HBM4",
+        "HBM bottleneck",
+        "memory shortage",
+    ]
+}
