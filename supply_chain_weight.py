51d23fd0a4917678a1b77bebb50b237a04318162
professionbob <professionbob@gmail.com>
2026-05-24 13:22:17 +0800

    Create supply_chain_weight.py

---
+++ supply_chain_weight.py
@@ -0,0 +1,22 @@
+# =========================
+# SUPPLY CHAIN WEIGHT
+# =========================
+
+SUPPLY_CHAIN_WEIGHT = {
+
+    "materials": 10,
+
+    "laser_drilling": 10,
+
+    "inspection": 9,
+
+    "substrate": 9,
+
+    "memory": 9,
+
+    "equipment": 7,
+
+    "networking": 7,
+
+    "packaging": 5,
+}
