fe631d3e7348df911191835c46296e9ecba92973
professionbob <professionbob@gmail.com>
2026-05-25 15:46:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from leaderboard_engine import build_leaderboard, build_sector_rotation
 # Telegram
 # =========================
 
-BOT_TOKEN = "你的Telegram BOT TOKEN"
+BOT_TOKEN = "8846284007:AAEZz4f50N8g1JcC6P8Z2ujcA2hx32-gv5A"
 CHAT_ID = "你的CHAT ID"
 
 
